4a51675ff3b066ac0a0b6d36e48dd7e858fcf997
Rishu Tripathi <rishutripathi@Rishus-MacBook-Air.local>
2024-11-15 08:30:42 +0530

    api-gateway_v2

--- libs/interface/src/interface.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InterfaceService } from './interface.service';
-
-@Module({
-  providers: [InterfaceService],
-  exports: [InterfaceService],
-})
-export class InterfaceModule {}
