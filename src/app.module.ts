5de9c1cc5fdfb8af1c14a4309587f46673ae9b22
Rishu Tripathi <rishutripathi@Rishus-MacBook-Air.local>
2024-11-15 08:25:41 +0530

    api-gatewy:first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,45 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller/app.controller';
+import { AppService } from './service/app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ApiGatewayController } from './controller/api-gateway.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../../.env',
+      isGlobal: true
+    }),
+    ClientsModule.register([
+      {
+        name: 'AUTH_SVC',
+        transport: Transport.TCP,
+        options: {
+          host: 'localhost',
+          port: Number(process.env.AUTH_SVC_PORT)
+        }
+      },
+      {
+        name: 'POST_SVC',
+        transport: Transport.TCP,
+        options: {
+          host: 'localhost',
+          port: Number(process.env.POST_SVC_PORT)
+        }
+      },
+      {
+        name: 'USER_SVC',
+        transport: Transport.TCP,
+        options: {
+          host: 'localhost',
+          port: Number(process.env.USER_SVC_PORT)
+        }
+      }
+    ])
+  ],
+  controllers: [AppController, ApiGatewayController],
   providers: [AppService],
 })
 export class AppModule {}
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
+  const apiGatewayPort = Number(process.env.API_GATEWAY_PORT);
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice(
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: apiGatewayPort
+      }
+    }
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
+  await app.startAllMicroservices();
+  console.log("all microservices started");
+  await app.listen(apiGatewayPort, () => console.log("api-gateway is listening on PORT:", apiGatewayPort));
 }
 bootstrap();
